6c0d065be30515e9785091c5491bcdb46b1e8b12
Milad Raeisi <miladsoft@yahoo.com>
2019-06-16 21:12:46 +0430

    First Commit

---
+++ SimpleFormGenerator.DataLayer/Context/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace SimpleFormGenerator.DataLayer.Context
+{
+    public interface IUnitOfWork
+    {
+        IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+        int SaveAllChanges();
+        void MarkAsChanged<TEntity>(TEntity entity) where TEntity : class;
+        IList<T> GetRows<T>(string sql, params object[] parameters) where T : class;
+    }
+}
---
+++ SimpleFormGenerator.DomainClasses/FieldValidation.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleFormGenerator.DomainClasses
+{
+    public class FieldValidation
+    {
+        public int Id { get; set; }
+        public string Rule { get; set; }
+        public virtual Field Field { get; set; }
+    }
+}
